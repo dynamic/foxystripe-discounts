c658ca1bfe1bde16516c5bcffacaa1948e42ec81
Jason Irish <jirish@dynamicagency.com>
2016-11-30 23:08:42 -0600

    CMS cleanup

--- code/FoxyStripeDiscountManager.php
+++ code/FoxyStripeDiscountManager.php
@@ -21,6 +21,17 @@ class FoxyStripeDiscountManager extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
+        if (!$this->owner->DiscountTitle && $this->owner->ProductDiscountTiers()->exists()) {
+            $fields->addFieldTotab(
+                'Root.Discounts',
+                new LiteralField(
+                    "ProductDiscountHeaderWarning",
+                    "<p class=\"message warning\">A discount title is required for FoxyCart to properly parse the value. 
+                        The discounts will not be applied until a title is entered.</p>"
+                )
+            );
+        }
+
         $fields->addFieldToTab(
             'Root.Discounts',
             TextField::create('DiscountTitle')->setTitle(_t('Product.DiscountTitle', 'Discount Title'))
@@ -32,7 +43,7 @@ class FoxyStripeDiscountManager extends DataExtension
             $config->removeComponentsByType('GridFieldAddExistingAutocompleter');
             $config->removeComponentsByType('GridFieldDeleteAction');
             $config->addComponent(new GridFieldDeleteAction(false));
-            $discountGrid = GridField::create('ProductDiscountTiers', 'Product Discounts', $this->ProductDiscountTiers(), $config);
+            $discountGrid = GridField::create('ProductDiscountTiers', 'Product Discounts', $this->owner->ProductDiscountTiers(), $config);
             $fields->addFieldToTab('Root.Discounts', $discountGrid);
         }
     }
